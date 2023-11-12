81d325d194bbc4e523270d6bb1b4ba7b6cbf4161
Duong <duong123@gmail.com>
2023-11-12 15:18:51 +0700

    Landau

--- src/com/acerstore/app/dao/AcerDAO.java
+++ src/com/acerstore/app/dao/AcerDAO.java
@@ -4,10 +4,17 @@
  */
 package com.acerstore.app.dao;
 
+import java.util.List;
+
 /**
  *
  * @author PC
  */
-public class AcerDAO {
-    
+public abstract class AcerDAO<EntityType, KeyType>{
+    public abstract void insert(EntityType entity);
+    public abstract void update(EntityType entity);
+    public abstract void delete(KeyType id);
+    public abstract List<EntityType> selectAll();
+    public abstract EntityType selectById(KeyType id);
+    public abstract List<EntityType> selectBySql(String sql, Object...args);
 }
